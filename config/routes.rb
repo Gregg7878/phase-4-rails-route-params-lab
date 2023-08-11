ce54c823d6fef4c2daf8e1d4b2ef72f3a6f8c52d
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-08-11 14:20:03 +0300

    modified students_controller.rb modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :students, only: [:show]
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
